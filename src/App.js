fbdce607e347f6fc92384a67e0a1108ef09eb128
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-07-06 20:59:40 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World</h1>
+      <div>{count}</div>
+      {/* This method avoids race conditions */}
+      <button onClick={() => setCount(currentCount => currentCount + 1)}>
+        +
+      </button>
     </div>
   );
 }
